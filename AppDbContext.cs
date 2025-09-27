4d0b903c5d2cf8c34bb94763a4a6cd58a6248616
Nitya N Hegde <108052615+Nitya-N-Hegde20@users.noreply.github.com>
2025-09-27 15:51:15 +0530

    Add files via upload
    Added other files

---
+++ AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HealthCompass.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace HealthCompass
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
